f1e5917ce39b5c12edf89bd4f783c4af9d634219
Monica Palko <m.a.palko07@gmail.com>
2024-02-18 19:04:18 -0500

    -a

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,13 @@
-x
+const sentence = (text) => {
+let delay = 0;
+
+  for (const char of text) { 
+    setTimeout(() => {
+    process.stdout.write(char); + '\n'
+  }, delay);
+  delay += 50;
+  } 
+};
+
+const text = "hello there from lighthouse labs";
+sentence(text);
